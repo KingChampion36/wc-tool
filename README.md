353824ca398ee719279691127a2123771e6bb559
Rahul <26169430+KingChampion36@users.noreply.github.com>
2023-12-28 12:30:57 +0530

    feat: Word Count Tool

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
 # wc-tool
+
+**Coding Challenge:** https://codingchallenges.fyi/challenges/challenge-wc/
+
+## Linux `wc` command
+
+The functional requirements for wc are concisely described by it’s man page - give it a go in your local terminal now:
+
+```shell
+man wc
+```
+
+### Size of a file
+
+```shell
+wc -c test.txt
+```
+
+### Number of lines in a file
+
+```shell
+wc -l test.txt
+```
+
+### Number of words in a file
+
+```shell
+wc -w test.txt
+```
+
+### Number of characters in a file
+
+```shell
+wc -m test.txt
+```
+
+## Compile and Run
+
+### Compile
+
+```shell
+javac WordCount.java
+```
+
+### Run
+
+#### 1. Size of a file
+
+```shell
+java WordCount -c test.txt
+```
+
+#### 2. Number of lines in a file
+
+```shell
+java WordCount -l test.txt
+```
+
+#### 3. Number of words in a file
+
+```shell
+java WordCount -w test.txt
+```
+
+#### 4. Number of characters in a file
+
+```shell
+java WordCount -m test.txt
+```
+
+#### 5. Default option
+
+```shell
+java WordCount test.txt
+```
+
+#### 6. No file name is specified (Take user input in this case) and print size
+
+```shell
+java WordCount -c
+```
+
+#### 7. No file name is specified (Take user input in this case) and print number of lines
+
+```shell
+java WordCount -l
+```
+
+#### 8. No file name is specified (Take user input in this case) and print number of words
+
+```shell
+java WordCount -w
+```
+
+#### 9. No file name is specified (Take user input in this case) and print number of characters
+
+```shell
+java WordCount -m
+```
+
+#### 10. No file name is specified (Take user input in this case) and default behaviour
+
+```shell
+java WordCount
+```
---
+++ WordCount.java
@@ -0,0 +1,134 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class WordCount {
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      final String userInput = takeUserInput();
+      System.out.println(numberOfLinesInAString(userInput) + " " + numberOfWordsInAString(userInput) + " " + userInput.length());
+      return;
+    }
+
+    if (args.length == 1) {
+      final File file = new File(args[0]);
+      if (!file.exists() || !file.isFile()) {
+        takeUserInputAndOutputResult(args[0]);
+      } else {
+        System.out.println(numberOfLinesInAFile(args[0]) + " " + numberOfWordsInAFile(args[0]) + " " + file.length());
+      }
+      return;
+    }
+
+    final String filePath = args[1];
+    final File file = new File(filePath);
+    if (!file.exists() || !file.isFile()) {
+      throw new IllegalArgumentException("File is invalid or it doesn't exist");
+    }
+    switch (args[0]) {
+      case "-c" -> System.out.println(file.length());
+      case "-l" -> System.out.println(numberOfLinesInAFile(filePath));
+      case "-w" -> System.out.println(numberOfWordsInAFile(filePath));
+      case "-m" -> System.out.println(numberOfCharactersInAFile(filePath));
+      default -> throw new IllegalArgumentException("Invalid argument");
+    }
+    ;
+  }
+
+  private static void takeUserInputAndOutputResult(String arg) {
+    final String userInput = takeUserInput();
+    switch (arg) {
+      case "-c" -> System.out.println(userInput.length());
+      case "-l" -> System.out.println(numberOfLinesInAString(userInput));
+      case "-w" -> System.out.println(numberOfWordsInAString(userInput));
+      case "-m" -> System.out.println(userInput.length());
+      default -> throw new IllegalArgumentException("Invalid argument");
+    }
+    ;
+  }
+
+  private static String takeUserInput() {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String userInput = "";
+    try {
+      userInput = reader.readLine();
+    } catch (Exception e) {
+
+    }
+    return userInput;
+  }
+
+  private static int numberOfLinesInAFile(String filePath) {
+    int lines = 0;
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(filePath));
+      while (reader.readLine() != null) {
+        lines++;
+      }
+      reader.close();
+    } catch (IOException e) {
+      System.out.println("IOException thrown while counting number of lines in the file...");
+    }
+    return lines;
+  }
+
+  private static int numberOfLinesInAString(String text) {
+    if (text == null) {
+      return 0;
+    }
+    int lines = 1;
+    for (int i = 0; i < text.length(); i++) {
+      if (text.charAt(i) == '\n') {
+        lines++;
+      }
+    }
+    return lines;
+  }
+
+  private static int numberOfWordsInAFile(String filePath) {
+    int words = 0;
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(filePath));
+      String sentence = null;
+      do {
+        sentence = reader.readLine();
+        words += numberOfWordsInAString(sentence);
+      } while (sentence != null);
+      reader.close();
+    } catch (IOException e) {
+      System.out.println("IOException thrown while counting number of words in the file...");
+    }
+    return words;
+  }
+
+  private static int numberOfWordsInAString(String text) {
+    if (text == null || text.isEmpty()) {
+      return 0;
+    }
+    String[] words = text.split("\\s+");
+    int counter = 0;
+    for (int i = 0; i < words.length; i++) {
+      if (!words[i].isEmpty()) {
+        counter++;
+      }
+    }
+    return counter;
+  }
+
+  private static int numberOfCharactersInAFile(String filePath) {
+    int characterCount = 0;
+    try {
+      FileReader fileReader = new FileReader(filePath);
+      int data;
+      while ((data = fileReader.read()) != -1) {
+        characterCount++;
+      }
+      fileReader.close();
+    } catch (IOException e) {
+      System.out.println("IOException thrown while counting number of characters in the file...");
+    }
+    return characterCount;
+  }
+}
