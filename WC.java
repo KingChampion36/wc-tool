ef41788d9be51c0db3e81fe3cafbe499c53fc9f8
Rahul <26169430+KingChampion36@users.noreply.github.com>
2023-12-28 12:40:17 +0530

    fix: Rename file

--- WC.java
+++ WC.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class WordCount {
+public class WC {
   public static void main(String[] args) {
     if (args.length == 0) {
       final String userInput = takeUserInput();
